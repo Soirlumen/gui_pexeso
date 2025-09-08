1b1a636a7ae5aa0bb7b44a089a242ca829cf9a9b
unknown <novot129@fjfi.cvut.cz>
2025-09-08 11:56:09 +0200

    rungame changes

--- rungame.cpp
+++ rungame.cpp
@@ -1,12 +1,71 @@
 #include "rungame.h"
+#include <QHBoxLayout>
 #include <QVBoxLayout>
+#include <QLabel>
 #include <QPushButton>
+#include <QGraphicsView>
+#include <QGraphicsScene>
 #include <QString>
 
-runGame::runGame(QWidget *parent) {
-
-    layoutMain=new QVBoxLayout(this);
-    btnBackToMenu=new QPushButton(tr("Back to main menu"));
-    layoutMain->addWidget(btnBackToMenu);
+void runGame::setupConnects()
+{
     connect(btnBackToMenu, &QPushButton::clicked, this, &runGame::clickedBackToMenu);
+}
+
+void runGame::setupLayout()
+{
+    // Hlavní horizontální layout
+    layoutMain = new QHBoxLayout(this);
+
+    // Levý panel (menu + hráči + info)
+    layoutLeft = new QVBoxLayout();
+    btnBackToMenu = new QPushButton(tr("Back to menu"));
+
+    lblplayerAndScore = new QLabel(tr("players | score"));
+
+
+    layoutLeft->addWidget(btnBackToMenu);
+    layoutLeft->addWidget(lblplayerAndScore);
+    QStringList playerNames = {"player1", "player2", "player3", "player4"};
+
+    for (int i = 0; i < 4; ++i) {
+        QLabel *lbl = new QLabel(this);
+
+        if (i < numPlayers) {
+            lbl->setText(QString("%1 | %2").arg(playerNames[i]).arg(0));
+        } else {
+            lbl->setText(" "); // placeholder
+        }
+
+        layoutLeft->addWidget(lbl);
+        playerLabels.append(lbl);
+    }
+
+    lblPlayerTurnAndRound = new QLabel(
+        tr("%1's turn | round: 0").arg(playerNames[0])
+        );
+
+
+    layoutLeft->addStretch(); // vyplní prázdný prostor
+    layoutLeft->addWidget(lblPlayerTurnAndRound);
+
+    // Pravý panel (hrací plocha + nuke button)
+    layoutRight = new QVBoxLayout();
+    view = new QGraphicsView();
+    scene = new QGraphicsScene(this);
+    view->setScene(scene);
+
+    btnNukeEverything = new QPushButton(tr("DONT CLICK THIS OR IT BLOWS UP"));
+
+    layoutRight->addWidget(view, 1); // 1 = expand
+    layoutRight->addWidget(btnNukeEverything);
+
+    // Spojení levého a pravého panelu
+    layoutMain->addLayout(layoutLeft);
+    layoutMain->addLayout(layoutRight);
+}
+
+runGame::runGame(QWidget *parent) {
+    setupLayout();
+    setupConnects();
 }
--- rungame.h
+++ rungame.h
@@ -3,12 +3,34 @@
 
 #include <QWidget>
 class QVBoxLayout;
+class QHBoxLayout;
 class QPushButton;
+class QLabel;
+class QGraphicsView;
+class QGraphicsScene;
+
 class runGame : public QWidget
 {
     Q_OBJECT
-    QVBoxLayout *layoutMain;
+    QHBoxLayout *layoutMain;
+    QVBoxLayout *layoutLeft;
+    QVBoxLayout *layoutRight;
+    QLabel *lblplayerAndScore;
+    QLabel *lblPlayerTurnAndRound;
     QPushButton* btnBackToMenu;
+    QPushButton* btnNukeEverything;
+
+    QGraphicsView *view;
+    QGraphicsScene *scene;
+
+    int numPlayers = 3;
+    QVector<QLabel*> playerLabels;
+
+    void setupConnects();
+    void setupLayout();
+    void redrawRightLayout();
+    void redrawPexeso();
+
 public:
     runGame(QWidget *parent = nullptr);
 
